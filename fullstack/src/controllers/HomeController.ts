883342910063a0b9a4d8b8870261583234359fbe
Alejandro Arteaga Herrera <alejoarteh05@MacBook-Air-de-Alejandro.local>
2026-02-04 11:15:03 -0500

    Tutorial 2

--- fullstack/src/controllers/HomeController.ts
+++ fullstack/src/controllers/HomeController.ts
@@ -1,4 +1,7 @@
 import type { Request, Response } from 'express';
+import { books } from '../data/Books.js';
+import { Book } from '../models/Books.js';
+
 export class HomeController {
     static index(req: Request, res: Response): void {
         const viewData: { [key: string]: any } = {};
@@ -14,6 +17,15 @@ export class HomeController {
         const viewData: { [key: string]: any } = {};
         viewData['title'] = 'Contact';
         res.render('home/contact', { viewData: viewData });
+    }
+    static Main_Point(req: Request, res: any){
+        const viewData: any = {};
+        viewData["books"] = books;
+        res.render('home/books', viewData)
+    }
+    static show(req: any, res: any){
+        const book = Book.findById(books, parseInt(req.params.id));
+        res.render('home/show', {book: book});
     }
 
 }
